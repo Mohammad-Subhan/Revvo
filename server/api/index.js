68eff6681de53fa6159ceac3abb318842c05f3d6
Mohammad-Subhan <subhankhalid185@gmail.com>
2025-07-13 23:44:27 +0500

    test

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,14 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
-import connectDB from "./config/database.js"
-import userRouter from "./routes/userRoutes.js"
-import ownerRouter from "./routes/ownerRoutes.js"
-import bookingRouter from "./routes/bookingRoutes.js"
+import connectDB from "../config/database.js"
+import userRouter from "../routes/userRoutes.js"
+import ownerRouter from "../routes/ownerRoutes.js"
+import bookingRouter from "../routes/bookingRoutes.js"
+import serverless from "serverless-http"
 
 // PORT configuration
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 // Initialize the express app
 const app = express();
@@ -27,4 +28,5 @@ app.use("/api/user", userRouter);
 app.use("/api/owner", ownerRouter);
 app.use("/api/bookings", bookingRouter);
 
-app.listen(PORT, () => console.log(`SUCCESS: Server is running on port ${PORT}`)); 
+// app.listen(PORT, () => console.log(`SUCCESS: Server is running on port ${PORT}`));
+export const handler = serverless(app);
