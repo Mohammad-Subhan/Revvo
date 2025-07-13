3efe6fd9a7b24100f96072ced78d194b13d431df
Mohammad-Subhan <subhankhalid185@gmail.com>
2025-07-13 21:19:50 +0500

    Initial settings

--- server/config/database.js
+++ server/config/database.js
@@ -2,16 +2,8 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        if (mongoose.connection.readyState >= 1) {
-            return;
-        }
-
         mongoose.connection.on("connected", () => console.log("SUCCESS: Database connected successfully!"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/revvo`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 5000
-        });
+        await mongoose.connect(`${process.env.MONGODB_URI}/revvo`);
     } catch (error) {
         console.error("ERROR: Error connecting to MongoDB:", error.message);
     }
