ce5d0ddd80d0be7fe55fa1142776d2efc36dd736
Mohammad-Subhan <subhankhalid185@gmail.com>
2025-07-13 14:43:22 +0500

    Added changes to the backend

---
+++ server/api/server.js
@@ -0,0 +1,32 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import connectDB from "../config/database.js"
+import userRouter from "../routes/userRoutes.js"
+import ownerRouter from "../routes/ownerRoutes.js"
+import bookingRouter from "../routes/bookingRoutes.js"
+
+// PORT configuration
+const PORT = process.env.PORT || 3000;
+
+// Initialize the express app
+const app = express();
+
+// Connect to the database
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Server check route
+app.get("/", (req, res) => res.send("Server is running!"));
+
+// User routes
+app.use("/api/user", userRouter);
+app.use("/api/owner", ownerRouter);
+app.use("/api/bookings", bookingRouter);
+
+app.listen(PORT, () => console.log(`SUCCESS: Server is running on port ${PORT}`));
+
+export default app;
