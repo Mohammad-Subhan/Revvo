9fdd8a16ef05c064eae6fa3bd8d49c623418f01e
Mohammad-Subhan <subhankhalid185@gmail.com>
2025-12-04 12:16:05 +0500

    Changed the logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
             transition={{ duration: 0.5 }}
             className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-border-color relative transition-all ${location.pathname === "/" && "bg-light"}`}>
             <Link to="/">
-                <motion.img whileHover={{ scale: 1.05 }} src={assets.logo} alt="" />
+                <motion.img whileHover={{ scale: 1.05 }} src={assets.logo} width={110} alt="" />
             </Link>
 
             <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-border-color right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${isOpen ? "max-sm:translate-x-0" : "max-sm:translate-x-full"} `}>
